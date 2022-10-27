87a50f0b9a8b27e8e4577c440e2273bcf515b6ca
Anitha-J-8 <114467954+Anitha-J-8@users.noreply.github.com>
2022-10-27 03:51:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+function fun(){
+    var username=document.getElementById("id1").value;
+    var pass=document.getElementById("id2").value;
+    if(username=="Anitha08" && pass=="ani819")
+    {
+      alert("Successfully login!");
+    }
+    else{
+      alert("Login Failed!");
+    }
+  }
+window.onscroll = function () {
+  myfunction()
+};
+function myfunction() {
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("g1").className = "slideup";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("g2").className = "slideup";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("g3").className = "slideup";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("g4").className = "slideup";
+  }
+  if (document.documentElement.scrollTop > 100) {
+    document.getElementById("v1").className = "slidetop";
+  }
+  if (document.documentElement.scrollTop > 100) {
+    document.getElementById("v2").className = "slidetop";
+  }
+  if (document.documentElement.scrollTop > 100) {
+    document.getElementById("v3").className = "slidetop";
+  }
+  if (document.documentElement.scrollTop > 100) {
+    document.getElementById("v4").className = "slidetop";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("p1").className = "slideabove";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("p2").className = "slideabove";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("p3").className = "slideabove";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("p4").className = "slideabove";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("f1").className = "slidehigh";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("f2").className = "slidehigh";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("f3").className = "slidehigh";
+  }
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("f4").className = "slidehigh";
+  }
+}
